53af2b620a8c698cd0a346f3ed5c78c9e5fb5027
fonglh <fonglh@gmail.com>
2012-08-06 21:38:49 +0800

    Initial commit

---
+++ mbpc-feba-cpt.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: MBPC FEBA Custom Post Type Plugin
+Plugin URI: http://example.com/wordpress-plugins/my-plugin
+Description: Adds a custom post type for FEBA messages to the website
+Version: 1.0
+Author: fonglh
+Author URI: https://wpadventures.wordpress.com
+License: GPLv2
+*/
+
